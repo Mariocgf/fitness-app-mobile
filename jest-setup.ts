dcfb4e30f327e1ff3174a93a7fd50f67e1196983
Mario Gonzalez <mariocgf8@gmail.com>
2026-04-17 16:38:01 -0300

    preparacion base para los test

---
+++ jest-setup.ts
@@ -0,0 +1,6 @@
+// matchers are built into @testing-library/react-native v12.4+
+
+// You can add global mocks here for features like react-native-gesture-handler, react-native-reanimated, etc.
+// For example, if you use react-native-reanimated:
+// import 'react-native-reanimated/lib/reanimated2/jestUtils';
+// setUpTests();
